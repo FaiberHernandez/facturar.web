cffd08ff8e2ed4fafe3a27fda773c78f87a523f4
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-02-21 11:40:38 -0500

    actualizar y crear detalle factura

--- src/app/factura/detalle-factura-form/detalle-factura-form.component.ts
+++ src/app/factura/detalle-factura-form/detalle-factura-form.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DetalleFacturaFormValues } from '@models/factura/detalle-factura/detalle-factura-form-values.model';
 import { DetalleFactura } from '@models/factura/detalle-factura/detalle-factura.model';
 import { FacturaService } from '@services/factura.service';
 import { Subscription } from 'rxjs';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detalle-factura-form',
   standalone: true,
-  imports: [],
+  imports: [ConfirmDialogModule, ToastModule, ReactiveFormsModule, InputTextModule, InputNumberModule, ButtonModule, CommonModule],
   templateUrl: './detalle-factura-form.component.html',
-  styleUrl: './detalle-factura-form.component.scss'
+  styleUrl: './detalle-factura-form.component.scss',
+  providers: [MessageService]
 })
-export class DetalleFacturaFormComponent implements OnInit {
+export class DetalleFacturaFormComponent implements OnInit, OnDestroy {
   idFactura: number;
   detalleFacturaToEdit?: DetalleFactura;
   form: FormGroup;
 
   subscriptions: Subscription[];
 
-  constructor(private readonly _facturaService: FacturaService) {
-    this.idFactura = 0;
+  constructor(private readonly _facturaService: FacturaService, 
+    private readonly _messageService: MessageService, 
+    public  dynamicDialogRef: DynamicDialogRef,
+    private readonly _dynamicDialogConfig: DynamicDialogConfig
+  ) {
+    this.idFactura = this._dynamicDialogConfig.data.facturaId;
+    this.detalleFacturaToEdit = this._dynamicDialogConfig.data.detalleFactura;
     this.form = this.createForm();
     this.subscriptions = [];
   }
@@ -31,11 +45,15 @@ export class DetalleFacturaFormComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
   createForm(): FormGroup {
     return new FormGroup({
-      producto: new FormControl('', Validators.required),
-      cantidad: new FormControl('', Validators.required),
-      precioUnitario: new FormControl('', Validators.required)
+      producto: new FormControl(null, [Validators.required, Validators.maxLength(100)]),
+      cantidad: new FormControl(null, [Validators.required, Validators.min(1)]),
+      precioUnitario: new FormControl(null, [Validators.required, Validators.min(0.01)])
     });
   }
 
@@ -43,16 +61,55 @@ export class DetalleFacturaFormComponent implements OnInit {
     this.form.patchValue(this.detalleFacturaToEdit!);
   }
 
+  saveDetalleFactura(): void {
+    if(this.detalleFacturaToEdit) {
+      this.updateDetalleFactura();
+    }else{
+      this.createDetalleFactura();
+    }
+  }
+
   createDetalleFactura(): void {
     const createParams: DetalleFacturaFormValues = this.form.value;
     this.subscriptions.push(this._facturaService.createDetalleFactura(this.idFactura, createParams).subscribe({
       next: (detalleFacturaId: number) => {
-        console.log('Detalle factura creado', detalleFacturaId);
+        this.handleCreateDetalleFactura(detalleFacturaId, createParams);
       },
-      error: (error) => {
-        console.error('Error al crear el detalle factura', error);
+      error: () => {
+        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al crear el detalle de factura', life: 3000, key: 'detalle-factura-form' });
       }
     }));
+  }
+
+  handleCreateDetalleFactura(detalleFacturaId: number, createParams: DetalleFacturaFormValues): void {
+    const detalleFactura: DetalleFactura = {
+      id: detalleFacturaId,
+      ...createParams,
+      facturaId: this.idFactura,
+      subtotal: createParams.cantidad * createParams.precioUnitario
+    }
+    this.dynamicDialogRef.close(detalleFactura);
+  }
+
+  updateDetalleFactura(): void {
+    const updateParams: DetalleFacturaFormValues = this.form.value;
+    this.subscriptions.push(this._facturaService.updateDetalleFactura(this.detalleFacturaToEdit!.id, updateParams).subscribe({
+      next: () => {
+        this.handleUpdateDetalleFactura(updateParams);
+      },
+      error: () => {
+        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al actualizar el detalle de factura', life: 3000, key: 'detalle-factura-form' });
+      }
+    }));
+  }
+
+  handleUpdateDetalleFactura(updateParams: DetalleFacturaFormValues): void {
+    const updatedDetalleFactura: DetalleFactura = {
+      ...this.detalleFacturaToEdit!,
+      ...updateParams,
+      subtotal: updateParams.cantidad * updateParams.precioUnitario
+    }
+    this.dynamicDialogRef.close(updatedDetalleFactura);
   }
   
   get producto() { return this.form.get('producto'); }
--- src/app/factura/listado-facturas/listado-facturas.component.ts
+++ src/app/factura/listado-facturas/listado-facturas.component.ts
@@ -11,6 +11,8 @@ import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule} from 'primeng/toast';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { DetalleFactura } from '@models/factura/detalle-factura/detalle-factura.model';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DetalleFacturaFormComponent } from '../detalle-factura-form/detalle-factura-form.component';
 
 @Component({
   selector: 'app-listado-facturas',
@@ -18,15 +20,17 @@ import { DetalleFactura } from '@models/factura/detalle-factura/detalle-factura.
   imports: [ButtonModule, CommonModule, TableModule, FormsModule, InputTextModule, ConfirmDialogModule, ToastModule],
   templateUrl: './listado-facturas.component.html',
   styleUrl: './listado-facturas.component.scss',
-  providers: [ConfirmationService, MessageService]
+  providers: [ConfirmationService, MessageService, DialogService]
 })
 export class ListadoFacturasComponent implements OnInit, OnDestroy {
   facturas: Factura[];
   subscriptions: Subscription[];
+  dynamicDialogRef?: DynamicDialogRef;
 
   constructor(private readonly _facturaService: FacturaService,
     private readonly _confirmationService: ConfirmationService,
-    private readonly _messageService: MessageService
+    private readonly _messageService: MessageService,
+    private readonly _dialogService: DialogService
   ) {
     this.facturas = [];
     this.subscriptions = [];
@@ -38,15 +42,13 @@ export class ListadoFacturasComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.dynamicDialogRef?.destroy();
   }
 
   getFacturas(): void {
     this.subscriptions.push(this._facturaService.getFacturas().subscribe({
       next: (listadoFacturas: Factura[]) => {
         this.facturas = listadoFacturas;
-      },
-      error: (error) => {
-        console.error('Error al obtener las facturas', error);
       }
     }));
   }
@@ -78,6 +80,7 @@ export class ListadoFacturasComponent implements OnInit, OnDestroy {
       rejectLabel:"No",
       rejectIcon:"none",
       rejectButtonStyleClass:"p-button-text",
+      key: 'listado-facturas',
       accept: () => {
         this.deleteDetalleFactura(detalleFactura);
       }
@@ -88,10 +91,10 @@ export class ListadoFacturasComponent implements OnInit, OnDestroy {
     this.subscriptions.push(this._facturaService.deleteFactura(facturaId).subscribe({
       next: () => {
         this.facturas = this.facturas.filter(factura => factura.id !== facturaId);
-        this._messageService.add({ severity: 'info', summary: 'Acción ejecutada con exíto', detail: 'Se ha eliminado la factura satisfactoriamente' });
+        this._messageService.add({ severity: 'success', summary: 'Acción ejecutada con exíto', detail: 'Se ha eliminado la factura satisfactoriamente', key: 'listado-facturas-toast' });
       },
       error: () => {
-        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la factura', life: 3000 });
+        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la factura', life: 3000, key: 'listado-facturas-toast' });
       }
     }));
   }
@@ -105,16 +108,85 @@ export class ListadoFacturasComponent implements OnInit, OnDestroy {
             factura.total = this.getTotalFactura(factura);
           }
         });
-        this._messageService.add({ severity: 'info', summary: 'Acción ejecutada conéxito', detail: 'Se ha eliminado el detalle de la factura satisfactoriamente' });
+        this._messageService.add({ severity: 'success', summary: 'Acción ejecutada con éxito', detail: 'Se ha eliminado el detalle de la factura satisfactoriamente', key: 'listado-facturas-toast' });
       },
       error: () => {
-        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar el detalle de la factura', life: 3000 });
+        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar el detalle de la factura', life: 3000, key: 'listado-facturas-toast' });
       }
     }));
   }
 
   getTotalFactura(factura: Factura): number {
     return factura.detalles.reduce((acc, detalle) => acc + detalle.subtotal, 0);
+  }
+
+  createDetalleFactura(facturaId: number): void {
+    this.dynamicDialogRef = this._dialogService.open(DetalleFacturaFormComponent, {
+      header: 'Crear detalle de factura',
+      position: 'top',
+      width: '50vw',
+      data: { 
+        facturaId
+      },
+      dismissableMask: true,
+      modal: true,
+      breakpoints: {
+        '960px': '75vw',
+        '640px': '90vw'
+      },
+    });
+    this.onCloseCreateDetalleFactura();
+  }
+
+  private onCloseCreateDetalleFactura(): void {
+    this.subscriptions.push(this.dynamicDialogRef!.onClose.subscribe({
+      next: (detalleFactura? : DetalleFactura) => {
+        if (detalleFactura) {
+          this.facturas.forEach(factura => {
+            if (factura.id === detalleFactura.facturaId) {
+              factura.detalles = [...factura.detalles, detalleFactura];
+              factura.total = this.getTotalFactura(factura);
+              this._messageService.add({severity:'success', summary:'Detalle factura creado', detail:'Detalle factura creado satisfactoriamente', key: 'listado-facturas-toast'});
+            }
+          });
+        }
+      }
+    }));
+  }
+
+  editDetalleFactura(facturaId: number, detalleFactura: DetalleFactura): void {
+    this.dynamicDialogRef = this._dialogService.open(DetalleFacturaFormComponent, {
+      header: 'Editar detalle de factura',
+      position: 'top',
+      width: '50vw',
+      data: { 
+        facturaId,
+        detalleFactura
+      },
+      dismissableMask: true,
+      modal: true,
+      breakpoints: {
+        '960px': '75vw',
+        '640px': '90vw'
+      },
+    });
+    this.onCloseEditDetalleFactura();
+  }
+
+  private onCloseEditDetalleFactura(): void {
+    this.subscriptions.push(this.dynamicDialogRef!.onClose.subscribe({
+      next: (detalleFactura? : DetalleFactura) => {
+        if (detalleFactura) {
+          this.facturas.forEach(factura => {
+            if (factura.id === detalleFactura.facturaId) {
+              factura.detalles = factura.detalles.map(detalle => detalle.id === detalleFactura.id ? {...detalleFactura} : detalle);
+              factura.total = this.getTotalFactura(factura);
+              this._messageService.add({severity:'success', summary:'Detalle factura actualizado', detail:'Detalle factura actualizado satisfactoriamente', key: 'listado-facturas-toast'});
+            }
+          });
+        }
+      }
+    }));
   }
 
 }
