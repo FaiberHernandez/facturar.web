4f7b8f57de16acca69e28a60183325fa889bd696
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-02-21 05:47:43 -0500

    delete factura

--- src/app/factura/listado-facturas/listado-facturas.component.ts
+++ src/app/factura/listado-facturas/listado-facturas.component.ts
@@ -7,19 +7,26 @@ import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule} from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-listado-facturas',
   standalone: true,
-  imports: [ButtonModule, CommonModule, TableModule, FormsModule, InputTextModule],
+  imports: [ButtonModule, CommonModule, TableModule, FormsModule, InputTextModule, ConfirmDialogModule, ToastModule],
   templateUrl: './listado-facturas.component.html',
-  styleUrl: './listado-facturas.component.scss'
+  styleUrl: './listado-facturas.component.scss',
+  providers: [ConfirmationService, MessageService]
 })
 export class ListadoFacturasComponent implements OnInit, OnDestroy {
   facturas: Factura[];
   subscriptions: Subscription[];
 
-  constructor(private readonly _facturaService: FacturaService) {
+  constructor(private readonly _facturaService: FacturaService,
+    private readonly _confirmationService: ConfirmationService,
+    private readonly _messageService: MessageService
+  ) {
     this.facturas = [];
     this.subscriptions = [];
   }
@@ -39,6 +46,34 @@ export class ListadoFacturasComponent implements OnInit, OnDestroy {
       },
       error: (error) => {
         console.error('Error al obtener las facturas', error);
+      }
+    }));
+  }
+
+  deleteFacturaConfirmation(facturaId: number): void {
+    this._confirmationService.confirm({
+      message: '¿Está seguro de que desea eliminar la factura?',
+      header: 'Eliminar factura',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon:"none",
+      acceptLabel:"Sí",
+      rejectLabel:"No",
+      rejectIcon:"none",
+      rejectButtonStyleClass:"p-button-text",
+      accept: () => {
+        this.deleteFactura(facturaId);
+      }
+    });
+  }
+
+  deleteFactura(facturaId: number): void {
+    this.subscriptions.push(this._facturaService.deleteFactura(facturaId).subscribe({
+      next: () => {
+        this.facturas = this.facturas.filter(factura => factura.id !== facturaId);
+        this._messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
+      },
+      error: () => {
+        this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al eliminar la factura', life: 3000 });
       }
     }));
   }
