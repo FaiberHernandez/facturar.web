41b621437a9a9824f7bf6714fe78ea665da65211
Faiber <faiberhernandezas@unimagdalena.edu.co>
2025-02-21 03:14:40 -0500

    configurando rutas base

---
+++ src/app/factura/listado-facturas/listado-facturas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-facturas',
+  standalone: true,
+  imports: [],
+  templateUrl: './listado-facturas.component.html',
+  styleUrl: './listado-facturas.component.scss'
+})
+export class ListadoFacturasComponent {
+
+}
